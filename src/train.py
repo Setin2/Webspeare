ec365eec125425f5205967bca02fd56592848d9c
Setin2 <stefancernat88@yahoo.com>
2023-01-12 14:31:42 +0100

    trained models

--- src/train.py
+++ src/train.py
@@ -25,6 +25,10 @@ def loss_function(actual, pred):
 
     return loss
 
+def save_models():
+  encoder.save_weights('models/encoder.ckpt')
+  decoder.save_weights('models/decoder.ckpt')
+
 def train(num_epochs, steps_per_epoch, lr, batch_size, embed_dim, encode_units):
 
   @tf.function
@@ -64,13 +68,48 @@ def train(num_epochs, steps_per_epoch, lr, batch_size, embed_dim, encode_units):
       batch_loss = train_step(input_batch, target_batch, encoder_hidden)
       total_loss += batch_loss
 
-      #if batch % 50 == 0:
-      print('Epoch {} Batch {} Loss {:.4f}'.format(epoch, batch, batch_loss.numpy()))
+      if batch % 25 == 0:
+        save_models()
+        print('Epoch {} Batch {} Loss {:.4f}'.format(epoch, batch, batch_loss.numpy()))
 
-    # save checkpoint
-    checkpoint.save(file_prefix = checkpoint_prefix)
+    save_models()
 
     print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+
+def clean_sentence(text, MAX_LENGTH):
+    text = tokenize_corpus.clean(text)
+    text = input_lang.texts_to_sequences([text])
+    text = tf.keras.preprocessing.sequence.pad_sequences(text, maxlen=MAX_LENGTH, padding='post')
+    text = tf.convert_to_tensor(text, dtype=tf.int32)
+    return text
+
+def translate(sentence):
+  MAX_LENGTH = 16
+  sentence = clean_sentence(sentence, MAX_LENGTH)
+  result = ""
+
+  # initalize first encoder state
+  encoder_hidden = [tf.zeros((1, ENCODER_UNITS))]
+  encoder_out, encoder_hidden = encoder(sentence, encoder_hidden)
+
+  # feed start token into decoder
+  decoder_hidden = encoder_hidden
+  current_word = tf.expand_dims([target_lang.word_index['<start>']], 0)
+
+  # feed the decoder our sentence
+  for word_index in range(MAX_LENGTH):
+    logits, decoder_hidden, attention_weights = decoder(current_word, decoder_hidden, encoder_out)
+
+    predicted_id = tf.argmax(logits[0]).numpy()
+
+    if target_lang.index_word[predicted_id] == "<end>":
+      break
+
+    result += target_lang.index_word[predicted_id] + " "
+
+    current_word = tf.expand_dims([predicted_id], 0)
+
+  return result
 
 if __name__ == "__main__":
     # create Tensorflow dataset
@@ -90,11 +129,14 @@ if __name__ == "__main__":
     # define neural networks & optimizer
     encoder = Encoder(vocab_input_size, EMBEDDING_DIM, ENCODER_UNITS, BATCH_SIZE)
     decoder = Decoder(vocab_target_size, EMBEDDING_DIM, ENCODER_UNITS, BATCH_SIZE)
+
+    if os.path.isfile('./models/checkpoint'):
+      print("MODELS LOADED")
+      encoder.load_weights('models/encoder.ckpt')
+      decoder.load_weights('models/decoder.ckpt')
+
     optim = tf.keras.optimizers.Adam(learning_rate = LR)
 
-    # checkpoints for saving
-    checkpoint_dir = './training_checkpoints'
-    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-    checkpoint = tf.train.Checkpoint(optimizer = optim, encoder = encoder, decoder = decoder)
+    #train(NUM_EPOCHS, steps_per_epoch, LR, BATCH_SIZE, EMBEDDING_DIM, ENCODER_UNITS)
 
-    train(NUM_EPOCHS, steps_per_epoch, LR, BATCH_SIZE, EMBEDDING_DIM, ENCODER_UNITS)
+    print(translate("How are you?"))
